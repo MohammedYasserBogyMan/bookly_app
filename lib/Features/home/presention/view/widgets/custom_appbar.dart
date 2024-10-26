1f301d122b31b264633b619ccab90d0de45e8e62
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2024-10-26 12:46:29 +0300

    add custom appbar

---
+++ lib/Features/home/presention/view/widgets/custom_appbar.dart
@@ -0,0 +1,30 @@
+import 'package:bookl_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 22,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 26,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presention/view/widgets/home_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'custom_appbar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Column(
+        children: [CustomAppbar(), CustomListViewItem()],
+      ),
+    );
+  }
+}
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(''),
+    );
+  }
+}
