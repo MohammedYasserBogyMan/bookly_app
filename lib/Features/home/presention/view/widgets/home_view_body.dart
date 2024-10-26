30c0c38aefff59bde618370bba16f497e1bb4af0
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2024-10-26 12:59:21 +0300

    add custom list view item to home page

--- lib/Features/home/presention/view/widgets/home_view_body.dart
+++ lib/Features/home/presention/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookl_app/Features/home/presention/view/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_appbar.dart';
@@ -9,19 +10,11 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SafeArea(
       child: Column(
-        children: [CustomAppbar(), CustomListViewItem()],
+        children: [
+          CustomAppbar(),
+          CustomListViewItem(),
+        ],
       ),
-    );
-  }
-}
-
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Image.network(''),
     );
   }
 }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,5 @@
 class AssetsData {
   static const logo = 'assets/BOOKLY.png';
+  static const imageTest =
+      'https://www.designforwriters.com/wp-content/uploads/2017/10/design-for-writers-book-cover-tf-2-a-million-to-one.jpg';
 }
