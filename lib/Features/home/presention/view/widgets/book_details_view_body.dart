b7e85e04ae5289982b612ad591c74d062eecc2d2
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2024-11-05 18:17:09 +0200

    "Refactored BookDetailsViewBody widget to use CustomScrollView with SliverFillRemaining, removed import of FeatureBookListView, and rearranged widget layout."

--- lib/Features/home/presention/view/widgets/book_details_view_body.dart
+++ lib/Features/home/presention/view/widgets/book_details_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookl_app/Features/home/presention/view/widgets/book_action.dart';
 import 'package:bookl_app/Features/home/presention/view/widgets/custom_list_view_item.dart';
-import 'package:bookl_app/Features/home/presention/view/widgets/feature_book_list_view.dart';
 import 'package:bookl_app/Features/home/presention/view/widgets/similar_book_list_view.dart';
 import 'package:bookl_app/core/utils/styels.dart';
 import 'package:flutter/material.dart';
@@ -16,63 +15,71 @@ class BookDetailsViewBody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
 
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Column(
-          children: [
-            const CustomAppBarBookDetails(),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-              child: const FeaturedCustomListViewItem(),
-            ),
-            const SizedBox(
-              height: 43,
-            ),
-            const Text(
-              'A Million To One',
-              style: Styels.textStyel30,
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            Opacity(
-              opacity: .7,
-              child: Text(
-                'Tony Faggioli',
-                style: Styels.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                ),
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: [
+                  const CustomAppBarBookDetails(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+                    child: const FeaturedCustomListViewItem(),
+                  ),
+                  const SizedBox(
+                    height: 43,
+                  ),
+                  const Text(
+                    'A Million To One',
+                    style: Styels.textStyel30,
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Opacity(
+                    opacity: .7,
+                    child: Text(
+                      'Tony Faggioli',
+                      style: Styels.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  const BookAction(),
+                  const Expanded(
+                    child: SizedBox(
+                      height: 50,
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like',
+                      style: Styels.textStyel16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.width * .3,
+                      child: SimilarBooksListView()),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 18,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(
-              height: 37,
-            ),
-            const BookAction(),
-            const SizedBox(
-              height: 50,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Styels.textStyel16,
-              ),
-            ),
-            SizedBox(
-              height: 14,
-            ),
-            SizedBox(
-                height: MediaQuery.of(context).size.width * .3,
-                child: SimilarBooksListView()),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
