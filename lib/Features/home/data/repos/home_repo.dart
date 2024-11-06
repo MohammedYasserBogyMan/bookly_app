0e901b01df4708f5472afbd26b858c7421cf032e
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2024-11-06 07:15:55 +0200

    create home repo methods

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookl_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookl_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFuaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFuaturedBooks();
 }
