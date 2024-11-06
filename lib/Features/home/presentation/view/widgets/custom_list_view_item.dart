7854d70336ee4e8e676622d7a4af1d326508a4d2
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2024-11-06 12:28:38 +0200

    add cachedNetworkImage

--- lib/Features/home/presentation/view/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/view/widgets/custom_list_view_item.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class FeaturedCustomListViewItem extends StatelessWidget {
   const FeaturedCustomListViewItem({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-              imageUrl,
-            ),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) =>
+              const Icon(Icons.error_outline_rounded),
         ),
       ),
     );
